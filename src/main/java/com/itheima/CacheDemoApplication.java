64b698fd1384ade3df4f5d03dd95a7ce362f0949
Jane <994824764@qq.com>
2023-07-19 15:59:45 +0800

    @CachePut 将方法的返回值放到缓存中

--- src/main/java/com/itheima/CacheDemoApplication.java
+++ src/main/java/com/itheima/CacheDemoApplication.java
@@ -3,9 +3,11 @@ package com.itheima;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @Slf4j
 @SpringBootApplication
+@EnableCaching // 开启缓存注解功能
 public class CacheDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(CacheDemoApplication.class,args);
--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -4,6 +4,7 @@ import com.itheima.entity.User;
 import com.itheima.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,13 @@ public class UserController {
     private UserMapper userMapper;
 
     @PostMapping
+    // spEl spring Expression Language
+    @CachePut(cacheNames = "userCache",key = "#user.id") // 如果使用Spring Cache缓存数据，key的生成：userCache::2
+    //@CachePut(cacheNames = "userCache",key = "#result.id") // . 对象导航
+    //@CachePut(cacheNames = "userCache",key = "#p0.id") // p0 第一个参数
+    //@CachePut(cacheNames = "userCache",key = "#a0.id") // a0 第一个参数
+    //@CachePut(cacheNames = "userCache",key = "#root.args[0].id")
+
     public User save(@RequestBody User user){
         userMapper.insert(user);
         return user;
