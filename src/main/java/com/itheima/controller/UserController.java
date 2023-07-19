b45b5002d08364fb9c134a7c7eca017dc63bb567
Jane <994824764@qq.com>
2023-07-19 16:10:48 +0800

    @Cacheable - [200~在方法执行前先查询缓存中是否有数据，如果有数据，则直接返回缓存数据；如果没有缓存数据，调用方法并将方法返回值放到缓存中

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -5,6 +5,7 @@ import com.itheima.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,7 +23,6 @@ public class UserController {
     //@CachePut(cacheNames = "userCache",key = "#p0.id") // p0 第一个参数
     //@CachePut(cacheNames = "userCache",key = "#a0.id") // a0 第一个参数
     //@CachePut(cacheNames = "userCache",key = "#root.args[0].id")
-
     public User save(@RequestBody User user){
         userMapper.insert(user);
         return user;
@@ -39,6 +39,11 @@ public class UserController {
     }
 
     @GetMapping
+    // Spring Cache 底层基于代理技术，
+    // 一旦加入注解后，Spring Cache就会为当前这个Controller建立一个代理对象
+    // 在请求之前，先进入了代理对象，在代理对象中去查询redis
+    // 查询到后就直接返回 不会调用getById方法
+    @Cacheable(cacheNames = "userCache",key = "#id") // key的生成：userCache::2
     public User getById(Long id){
         User user = userMapper.getById(id);
         return user;
